02dc465c4c51de6c92a1c58f2bc1cfe435253490
Ifedolapo <Ifedolapo0001@gmail.com>
2026-02-12 23:45:47 +0100

    update store

--- store/useLightStore.ts
+++ store/useLightStore.ts
@@ -2,10 +2,14 @@ import { create } from "zustand";
 
 interface LightStore {
 	isMasterOn: boolean;
-	toggleMaster: () => void;
+	toggleMaster: () => void
+	isLampOn: boolean;
+	toggleLamp: () => void
 }
 
 export const useLightStore = create<LightStore>((set) => ({
 	isMasterOn: false,
 	toggleMaster: () => set((state) => ({ isMasterOn: !state.isMasterOn })),
+	isLampOn: false,
+	toggleLamp: () => set((state) => ({ isLampOn: !state.isLampOn })),
 }));
